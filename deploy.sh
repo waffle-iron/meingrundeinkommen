0638cc7696e4f0e29a5cb03224fd73458e9a9331
jottr <j@ovvvo.co>
2016-01-26 01:03:56 +0100

    Created another version of deployscript and circle config.

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,15 @@ SHA1=$1
 # Deploy image to Docker Hub
 docker push meinbge/meinbge-rails:$SHA1
 
+APP_NAME=meinbge-rails
 # Create new Elastic Beanstalk version
-EB_BUCKET=meinbge-rails-bucket
+EB_BUCKET="${APP_NAME}"-bucket
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name hello \
+aws elasticbeanstalk create-application-version --application-name "${APP_NAME}" \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name meinbge-env \
+aws elasticbeanstalk update-environment --environment-name "${APP_NAME}"-env \
     --version-label $SHA1
