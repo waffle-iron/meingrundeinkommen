fbd3603758548b1ab75427118b924da23df06567
jottr <j@ovvvo.co>
2016-02-20 00:15:35 +0000

    [rubocop] Style/Lambda

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
 
   has_many :flags, dependent: :destroy
 
-  scope :with_flag, lambda {|flag, value| joins(:flags).where("flags.name = ? and flags.value_boolean = ?", flag, value)}
+  scope :with_flag, ->(flag, value) { joins(:flags).where("flags.name = ? and flags.value_boolean = ?", flag, value)}
 
   #   # the participations in user_todos:
   has_many :user_wishes, dependent: :destroy
@@ -42,22 +42,22 @@ class User < ActiveRecord::Base
   validates :datenschutz, inclusion: [true]
 
   scope :byids, ->(ids) { where(['users.id IN (?)', ids.split(',')])}
-  scope :with_newsletter, lambda { where(newsletter: true) }
-  scope :without_newsletter, lambda { where(newsletter: false) }
-  scope :confirmed, lambda { where('confirmed_at is not null') }
-  scope :not_confirmed, lambda { where('confirmed_at is null') }
-  scope :participating, lambda { includes(:chances).where(chances: { confirmed: true })}
+  scope :with_newsletter, -> { where(newsletter: true) }
+  scope :without_newsletter, -> { where(newsletter: false) }
+  scope :confirmed, -> { where('confirmed_at is not null') }
+  scope :not_confirmed, -> { where('confirmed_at is null') }
+  scope :participating, -> { includes(:chances).where(chances: { confirmed: true })}
   scope :not_participating, -> { where.not(id: Chance.where(chances: { confirmed: true }).select(:user_id).uniq) }
-  scope :has_code, lambda { includes(:chances).where.not(chances: { code: nil })}
-  scope :with_crowdbar, lambda { includes(:flags).where(flags: {name: 'hasCrowdbar', value_boolean: true}) }
-  scope :without_crowdbar, lambda { includes(:flags).where(flags: {name: 'hasCrowdbar', value_boolean: false}) }
+  scope :has_code, -> { includes(:chances).where.not(chances: { code: nil })}
+  scope :with_crowdbar, -> { includes(:flags).where(flags: {name: 'hasCrowdbar', value_boolean: true}) }
+  scope :without_crowdbar, -> { includes(:flags).where(flags: {name: 'hasCrowdbar', value_boolean: false}) }
   scope :has_crowdcard, -> { joins(:crowdcards).distinct }
   scope :has_tandems, -> { where('users.id IN (SELECT DISTINCT(inviter_id) FROM tandems) OR users.id IN (SELECT DISTINCT(invitee_id) FROM tandems)') }
   scope :has_no_tandems, -> { where('users.id NOT IN (SELECT DISTINCT(inviter_id) FROM tandems where inviter_id is not null) AND users.id NOT IN (SELECT DISTINCT(invitee_id) FROM tandems where invitee_id is not null)') }
   scope :sign_up_after, ->(date) { where('created_at > ?',date)}
-  scope :is_squirrel, lambda { includes(:payment).where(payments: {active: true}) }
-  scope :frst_notification_not_sent, lambda { includes(:payment).where(payments: { sent_first_notification_at: nil }) }
-  scope :frst_notification_sent, lambda { includes(:payment).where.not(payments: { sent_first_notification_at: nil }) }
+  scope :is_squirrel, -> { includes(:payment).where(payments: {active: true}) }
+  scope :frst_notification_not_sent, -> { includes(:payment).where(payments: { sent_first_notification_at: nil }) }
+  scope :frst_notification_sent, -> { includes(:payment).where.not(payments: { sent_first_notification_at: nil }) }
   scope :last_squirrel_id, ->(last_squirrel_id) { includes(:payment).where(payments: { id: 0..last_squirrel_id.to_i }) }
 
   def self.all_newsletter_receipients
