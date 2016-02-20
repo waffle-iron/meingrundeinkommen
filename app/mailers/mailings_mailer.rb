07b0ffbbe116ba4fd798f85c75f378c11f918cd5
jottr <j@ovvvo.co>
2016-02-20 14:37:26 +0000

    Send transactionmals from `noreply@mein-grundeinkommen.de` Closes #28

--- app/mailers/mailings_mailer.rb
+++ app/mailers/mailings_mailer.rb
@@ -39,7 +39,7 @@ class MailingsMailer < MassMandrill::MandrillMailer
     template_content = [{ name: 'body', content: content }]
 
     mail(to:               addresses,
-         from:             'Mein Grundeinkommen <micha@meinbge.de>',
+         from:             'Mein Grundeinkommen <noreply@meinbge.de>',
          subject:          subject,
          template_content: template_content,
          template:         template,
