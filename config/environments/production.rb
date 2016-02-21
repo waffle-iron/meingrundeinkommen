3e7470e5ecc2481abcf7c7c32b8173bdc2bce054
jottr <j@ovvvo.co>
2016-02-21 15:40:44 +0000

    Set up CORS for font assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,10 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "d27upe1ug8gwpm.cloudfront.net"
+  config.action_controller.asset_host = 'd27upe1ug8gwpm.cloudfront.net'
+
+  # Set up CORS HTTP Headers headers to allow serving of fonts via cloudfront
+  config.font_assets.origin = '*'
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
