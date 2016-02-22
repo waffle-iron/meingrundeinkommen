b49214800c59c49e35b0b3860c1e63be9d196421
jottr <j@ovvvo.co>
2016-02-22 10:07:14 +0000

    Store clockwork files in s3 `storage/` dir

--- app/uploaders/storage_uploader.rb
+++ app/uploaders/storage_uploader.rb
@@ -12,7 +12,8 @@ class StorageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "storage/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    #"storage/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "storage/"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
