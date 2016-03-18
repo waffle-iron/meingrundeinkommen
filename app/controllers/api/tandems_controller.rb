4c15a5620f61ec7e43dd6e0357be08f6ca5559df
jottr <j@ovvvo.co>
2016-03-18 14:35:21 +0000

    Formatting

--- app/controllers/api/tandems_controller.rb
+++ app/controllers/api/tandems_controller.rb
@@ -3,16 +3,16 @@ class Api::TandemsController < ApplicationController
   def index
     x = []
     Tandem.where("invitee_id != inviter_id AND
-					  inviter_id is not null AND
-					  invitee_id is not null AND
-					  (
-					  	(invitee_grudges_inviter_for is not null AND
-					  	 invitee_grudges_inviter_for != '')
-						or
-						(inviter_grudges_invitee_for is not null AND
-						 inviter_grudges_invitee_for !='')
+            inviter_id is not null AND
+            invitee_id is not null AND
+            (
+              (invitee_grudges_inviter_for is not null AND
+               invitee_grudges_inviter_for != '')
+            or
+            (inviter_grudges_invitee_for is not null AND
+             inviter_grudges_invitee_for !='')
 
-		)").sample(50).map do |tandem|
+    )").sample(50).map do |tandem|
       inviter = User.find_by_id(tandem.inviter_id)
       invitee = User.find_by_id(tandem.invitee_id)
 
@@ -26,7 +26,7 @@ class Api::TandemsController < ApplicationController
           grudger: invitee,
           grudgee: inviter
         }
-   end
+      end
 
       if !tandem.inviter_grudges_invitee_for.nil? && tandem.inviter_grudges_invitee_for != ''
         grudges << {
@@ -34,7 +34,7 @@ class Api::TandemsController < ApplicationController
           grudger: inviter,
           grudgee: invitee
         }
-       end
+      end
 
       next if grudges.empty?
 
@@ -55,7 +55,7 @@ class Api::TandemsController < ApplicationController
       }
     end
     render json: x
-    end
+  end
 
   def create
     # todo:
@@ -69,46 +69,46 @@ class Api::TandemsController < ApplicationController
 
     # if tandem[:invitation_type] && (tandem[:invitee_id] == current_user.id || tandem[:inviter_id] == current_user.id) && current_user.tandems.count < 100
 
-    # 	tandem[:invitee_grudges_inviter_for] = grudge if tandem[:invitee_id] == current_user.id
-    # 	tandem[:inviter_grudges_invitee_for] = grudge if tandem[:inviter_id] == current_user.id
+    #   tandem[:invitee_grudges_inviter_for] = grudge if tandem[:invitee_id] == current_user.id
+    #   tandem[:inviter_grudges_invitee_for] = grudge if tandem[:inviter_id] == current_user.id
 
-    # 	tandem[:invitation_token] = loop do
-    # 	  token = SecureRandom.urlsafe_base64
-    # 	  break token unless Tandem.exists?(invitation_token: token)
-    # 	end
+    #   tandem[:invitation_token] = loop do
+    #     token = SecureRandom.urlsafe_base64
+    #     break token unless Tandem.exists?(invitation_token: token)
+    #   end
 
-    # 	#check if invited email address already exists in users and change to existing tandem
-    # 	if tandem[:invitation_type] == 'mail'
-    # 		partner = User.where(:email => tandem[:invitee_email]).first
-    # 		if partner
-    # 			tandem[:invitation_type] == 'existing'
-    # 			tandem[:invitee_id] == partner.id
-    # 		end
-    # 	end
+    #   #check if invited email address already exists in users and change to existing tandem
+    #   if tandem[:invitation_type] == 'mail'
+    #     partner = User.where(:email => tandem[:invitee_email]).first
+    #     if partner
+    #       tandem[:invitation_type] == 'existing'
+    #       tandem[:invitee_id] == partner.id
+    #     end
+    #   end
 
-    # 	if tandem[:invitation_type] == 'link'
-    # 		tandem[:invitation_accepted_at] = Time.now
-    # 		tandem[:invitee_participates] = true
-    # 		#send mail to inviter and inform about confirmation
-    # 		if r = Tandem.create(tandem)
-    # 			InvitationMailer.inform_about_link_confirmation(tandem,current_user,User.find(tandem[:inviter_id])).deliver
-    # 		end
-    # 	else #existing, mail, random
-    # 		tandem[:invitation_accepted_at] = Time.now
-    # 		if tandem[:invitation_type] == 'existing'
-    # 			invitee = User.find(tandem[:invitee_id])
-    # 			r[:invitee_participates] = true if invitee.chances.where(:confirmed => true).any?
-    # 			if r = Tandem.create(tandem)
-    # 				InvitationMailer.invite_existing(tandem,current_user,invitee).deliver
-    # 			end
-    # 		else
-    # 			r = Tandem.create(tandem)
-    # 		end
-    # 	end
+    #   if tandem[:invitation_type] == 'link'
+    #     tandem[:invitation_accepted_at] = Time.now
+    #     tandem[:invitee_participates] = true
+    #     #send mail to inviter and inform about confirmation
+    #     if r = Tandem.create(tandem)
+    #       InvitationMailer.inform_about_link_confirmation(tandem,current_user,User.find(tandem[:inviter_id])).deliver
+    #     end
+    #   else #existing, mail, random
+    #     tandem[:invitation_accepted_at] = Time.now
+    #     if tandem[:invitation_type] == 'existing'
+    #       invitee = User.find(tandem[:invitee_id])
+    #       r[:invitee_participates] = true if invitee.chances.where(:confirmed => true).any?
+    #       if r = Tandem.create(tandem)
+    #         InvitationMailer.invite_existing(tandem,current_user,invitee).deliver
+    #       end
+    #     else
+    #       r = Tandem.create(tandem)
+    #     end
+    #   end
 
     # end
 
-    #   	render json: {:tandem => r }
+    #     render json: {:tandem => r }
   end
 
   def update
