f2a23ad0710a18b47abab60a5fcd2f2d9fee7439
jottr <j@ovvvo.co>
2016-03-19 15:10:10 +0000

    rubocop -a

--- spec/models/crowdbar_spec.rb
+++ spec/models/crowdbar_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Crowdbar, type: :model do
   it 'retrieves the crowdbar data as json' do
     crowdbar = Crowdbar.new
-    # TODO Mock this. Maybe use VCR?
+    # TODO: Mock this. Maybe use VCR?
     crowdbar.fetch_stats('https://bar.mein-grundeinkommen.de/crowd_bar_stats.json')
 
     # expect(crowdbar.referal_count).to eq(217443)
