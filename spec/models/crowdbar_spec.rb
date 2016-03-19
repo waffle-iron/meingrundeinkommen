67658a0fbfa64c6ea06dca7354ec3b5ae87ee8d5
jottr <j@ovvvo.co>
2016-03-19 12:57:12 +0000

    Worked on crowdbar spec.

--- spec/models/crowdbar_spec.rb
+++ spec/models/crowdbar_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Crowdbar, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'retrieves the crowdbar data as json' do
+    crowdbar = Crowdbar.new
+    # TODO Mock this. Maybe use VCR?
+    crowdbar.fetch_stats('https://bar.mein-grundeinkommen.de/crowd_bar_stats.json')
+
+    # expect(crowdbar.referal_count).to eq(217443)
+    # expect(crowdbar.seven_day_commission).to eq(217443)
+    # expect(crowdbar.seven_day_referal_count).to eq(217443)
+    # expect(crowdbar.total_commission).to eq(217443)
+  end
 end
