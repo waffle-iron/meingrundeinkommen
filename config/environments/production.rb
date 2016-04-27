6031b9783a2699db9e26ad7be928fd28be2ac815
jottr <j@ovvvo.co>
2016-04-27 22:23:41 +0200

    Switch from Mandrill to Sparkpost (#45)
    * [gem] Removed mandrill and added sparkpost gems.
    
    * Set up sparkpost Mailer.
    
    * Set up sparkpost Mailer.
    
    Forgot to set up actionmailer. (#46)
    
    Sparkpost hotfix (#47)
    
    * Forgot to set up actionmailer.
    
    * Fixed Gemfile.
    
    ActionMailer: Fix 'from' domain (#48)
    
    New sparkpost API key. (#49)
    
    ActionMailer: Use smtp as delivery method. (#50)
    
    Don't force STARTTLS (#51)
    
    ActionMailer: Specify authentication method. (#52)
    
    ActionMailer: Re-enable STTARTTLS. (#53)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,11 +24,12 @@ Rails.application.configure do
     password: '6ef3cc88fada326360f9ac98a0a320d2ce27267b',
     address: 'smtp.sparkpostmail.com',
     port: 587,
-    #enable_starttls_auto: true,
+    enable_starttls_auto: :true,
     format: :html,
     :from => "\"Mein Grundeinkommen\" <support@mein-grundeinkommen.de>"
 
   }
+
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
