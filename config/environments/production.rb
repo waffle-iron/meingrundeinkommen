974d2a140b7c375cdd7169b8fcc699b72d55039d
jottr <j@ovvvo.co>
2016-04-28 03:18:26 +0200

    Don't explicitly enable starttls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
     password: '6ef3cc88fada326360f9ac98a0a320d2ce27267b',
     address: 'smtp.sparkpostmail.com',
     port: 587,
-    enable_starttls_auto: true,
+    #enable_starttls_auto: true,
     format: :html,
     :from => "\"Mein Grundeinkommen\" <support@mein-grundeinkommen.de>"
 
