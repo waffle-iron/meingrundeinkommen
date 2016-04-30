0f6671bfe2d275a158137b8a46b4815996680635
jottr <j@ovvvo.co>
2016-04-30 19:01:35 +0200

    [config] Fixed sparkpost api key.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     user_name: 'SMTP_Injection',
-    password: ENV['SPARKPOST_API'],
+    password: ENV['SPARKPOST_API_KEY'],
     address: 'smtp.sparkpostmail.com',
     port: 587,
     enable_starttls_auto: :true,
